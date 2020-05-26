1fe390499e6862ad62c2a68bcb468f38e2b17eb2
Alan Jhonnes <alanjhonnes@gmail.com>
2020-05-26 14:50:59 -0300

    feat: using web component

--- src/modules/buttons/buttons.module.ts
+++ src/modules/buttons/buttons.module.ts
@@ -1,3 +1,19 @@
 export const BUTTON_MODULE = 'sfw.button';
-
-angular.module(BUTTON_MODULE, [])
+console.log('before');
+const btnModule = angular.module(BUTTON_MODULE, ['ui.router']);
+console.log('after');
+btnModule.config(['$stateProvider', function($stateProvider: any) {
+    const helloState = {
+      name: 'hello',
+      template: `<h3>hello world!</h3>
+      `
+    }
+  
+    const aboutState = {
+      name: 'about',
+      template: '<h3>Its the UI-Router hello world app!</h3>'
+    }
+  
+    $stateProvider.state(helloState);
+    $stateProvider.state(aboutState);
+  }]);
--- src/modules/buttons/components/button/button.component.ts
+++ src/modules/buttons/components/button/button.component.ts
@@ -1,10 +1,10 @@
 import { BUTTON_MODULE } from "../../buttons.module";
 import './button.component.scss';
 
-
+console.log('before component');
 export class SafraWalletButton implements ng.IComponentController {
 
-    static readonly $inject = [];
+    static readonly $inject = ['$state'];
 
     text!: string;
 
@@ -12,15 +12,27 @@ export class SafraWalletButton implements ng.IComponentController {
 
     textos = [
         '1', '2', '3', '4'
-    ]
+    ];
+
+    texto = {
+        title: 'alan',
+    };
 
     add() {
         this.count += 1;
     }
 
-    constructor() {
+    onMessageReceived($event: any) {
+        console.log('message received: ', $event);
+    }
+
+    constructor(public $state: any) {
         console.log('safra wallet button initialized');
     } 
+
+    $onInit() {
+        window['journey-b'].bootstrapJourneyB();
+    }
 }
 
 angular.module(BUTTON_MODULE)
@@ -40,5 +52,17 @@ angular.module(BUTTON_MODULE)
 <div ng-repeat="texto in $ctrl.textos"> 
         {{texto}}
 </div>
+
+<a ui-sref="hello" ui-sref-active="active" ui-sref-opts="{ reload: true }">Hello</a>
+<a ui-sref="about" ui-sref-active="active" ui-sref-opts="{ reload: true }">About</a>
+
+<ui-view></ui-view>
+
+<journey-b-element 
+ng-prop-state_service="$ctrl.$state" 
+ng-prop-texto="$ctrl.texto" 
+ng-prop-textos="$ctrl.textos"
+ng-on-message_emitted="$ctrl.onMessageReceived($event)"
+></journey-b-element>
         `, 
     })
